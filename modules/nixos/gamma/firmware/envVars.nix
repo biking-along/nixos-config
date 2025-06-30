64347d2db5399b8d8c79c249daf28fa35b207a85
bikingalong <94024370+biking-along@users.noreply.github.com>
2025-06-29 21:07:51 -0500

    Update envVars.nix

--- modules/nixos/gamma/firmware/envVars.nix
+++ modules/nixos/gamma/firmware/envVars.nix
@@ -1,12 +1,13 @@
 {
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
-    MOZ_ENABLE_WAYLAND="1";
+    MOZ_ENABLE_WAYLAND = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
     GSK_RENDERER = "vulkan";
     LIBVA_DRIVER_NAME = "radeonsi";
     VDPAU_DRIVER = "radeonsi";
     AMD_VULKAN_ICD = "RADV";
+    RADV_TEX_ANISO = "16";
     XCURSOR_THEME = "Nordzy-catppuccin-mocha-dark";
     HYPRCURSOR_THEME = "Nordzy-hyprcursors-catppuccin-mocha-dark";
     XCURSOR_SIZE = "24";
