492246da6fb0f0630ee6fdc244820aa6f0df06a1
biking-along <rsw56379@gmail.com>
2025-06-29 22:35:33 -0500

    Fix kitty cfg formatting

--- modules/home-manager/gamma/kitty.nix
+++ modules/home-manager/gamma/kitty.nix
@@ -3,6 +3,26 @@
     enable = true;
     enableGitIntegration = true;
     shellIntegration.enableFishIntegration = true;
-    extraConfig = "shell fish\nconfirm_os_window_close 0\nmap ctrl+plus change_font_size all +1\nmap ctrl+equal change_font_size all +1\nmap ctrl+kp_add change_font_size all +1\nmap ctrl+minus change_font_size all -1\nmap ctrl+kp_subtract change_font_size all -1\nmap ctrl+underscore change_font_size all -1\nmap ctrl+0 change_font_size all 0\nmap ctrl+kp_0 change_font_size all 0\nrepaint_delay 0\ninput_delay 0\nsync_to_monitor yes\nenable_audio_bell yes\ntab_bar_min_tabs 1\ntab_bar_edge bottom\ntab_bar_style powerline\ntab_powerline_style slanted\ntab_title_template {title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+    extraConfig = ''
+      shell fish
+      confirm_os_window_close 0
+      map ctrl+plus change_font_size all +1
+      map ctrl+equal change_font_size all +1
+      map ctrl+kp_add change_font_size all +1
+      map ctrl+minus change_font_size all -1
+      map ctrl+kp_subtract change_font_size all -1
+      map ctrl+underscore change_font_size all -1
+      map ctrl+0 change_font_size all 0
+      map ctrl+kp_0 change_font_size all 0
+      repaint_delay 0
+      input_delay 0
+      sync_to_monitor yes
+      enable_audio_bell yes
+      tab_bar_min_tabs 1
+      tab_bar_edge bottom
+      tab_bar_style powerline
+      tab_powerline_style slanted
+      tab_title_template {title}{" :{}:".format(num_windows) if num_windows > 1 else ""}
+    '';
   };
 }
