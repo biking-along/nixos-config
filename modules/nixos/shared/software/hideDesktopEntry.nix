0559b1c96750fc528031bf3ea3c03fcffdf96a11
bikingalong <bikingalong@pm.me>
2025-07-09 12:18:30 -0500

    Edit desktop entry hiding module again

--- modules/nixos/shared/software/hideDesktopEntry.nix
+++ modules/nixos/shared/software/hideDesktopEntry.nix
@@ -5,18 +5,25 @@
   environment.systemPackages = [
 
     # Hide Neovim desktop entry
-    (lib.hiPrio (pkgs.runCommand "nvim.desktop-hide" { } ''
+    (lib.hiPrio (pkgs.runCommand "desktopEntryHide" { } ''
       mkdir -p "$out/share/applications"
+
       cat "${config.programs.neovim.package}/share/applications/nvim.desktop" > "$out/share/applications/nvim.desktop"
-      echo "Hidden=1;" >> "$out/share/applications/nvim.desktop"
+      echo "Hidden=1" >> "$out/share/applications/nvim.desktop"
+
+      cat "${config.programs.fish.package}/share/applications/fish.desktop" > "$out/share/applications/fish.desktop"
+      echo "Hidden=1" >> "$out/share/applications/fish.desktop"
+
+      cat "${config.programs.yazi.package}/share/applications/yazi.desktop" > "$out/share/applications/yazi.desktop"
+      echo "Hidden=1" >> "$out/share/applications/yazi.desktop"
     ''))
 
     # Hide fish desktop entry
-    (lib.hiPrio (pkgs.runCommand "fish.desktop-hide" { } ''
-      mkdir -p "$out/share/applications"
-      cat "${config.programs.fish.package}/share/applications/fish.desktop" > "$out/share/applications/fish.desktop"
-      echo "Hidden=1;" >> "$out/share/applications/fish.desktop"
-    ''))
+    # (lib.hiPrio (pkgs.runCommand "fish-desktop-hide" { } ''
+      # mkdir -p "$out/share/applications"
+      # cat "${config.programs.fish.package}/share/applications/fish.desktop" > "$out/share/applications/fish.desktop"
+      # echo "Hidden=1" >> "$out/share/applications/fish.desktop"
+    # ''))
 
   ];
 }
