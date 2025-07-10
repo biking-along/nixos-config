7903205a4fd766c1d4918d272c24381d8100e53e
bikingalong <bikingalong@pm.me>
2025-07-09 23:21:25 -0500

    Typo

--- modules/nixos/shared/nix/nvf.nix
+++ modules/nixos/shared/nix/nvf.nix
@@ -12,7 +12,7 @@
           {
             key = "<leader>ff";
             mode = "n";
-            action = ":lua MiniFiles.open()";
+            action = "<cmd>MiniFiles.open()<CR>";
             lua = true;
           }
         ];
