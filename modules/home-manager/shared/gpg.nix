567aeb776cb15a24d26adfe5aaf353b5916d335f
bikingalong <bikingalong@pm.me>
2025-07-11 02:55:45 -0500

    Gpg config

--- modules/home-manager/shared/gpg.nix
+++ modules/home-manager/shared/gpg.nix
@@ -1,9 +1,16 @@
 {pkgs, ...}: {
   programs.gpg = {
     enable = true;
+    settings = {
+      with-keygrip = true;
+      with-fingerprint = true;
+      keyid-format = "long";
+    };
   };
   services.gpg-agent = {
     enable = true;
+    enableFishIntegration = true;
+    enableSshSupport = true;
     pinentry.package = pkgs.pinentry-gnome3;
   };
 }
