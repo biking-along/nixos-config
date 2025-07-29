c66de8a82a8b6de72dca60dbaf1fb3bcecb163f3
biking-along <rsw56379@gmail.com>
2025-07-29 14:23:07 -0500

    Add firmware stuff

--- modules/nixos/gamma/firmware/firmware.nix
+++ modules/nixos/gamma/firmware/firmware.nix
@@ -1,9 +1,13 @@
-{
+{pkgs, ...}: {
   hardware = {
     enableAllFirmware = true;
     enableRedistributableFirmware = true;
     i2c.enable = true;
   };
 
-  services.fwupd.enable = true;
+  services = {
+    fwupd.enable = true;
+    dbus.packages = [pkgs.fwupd];
+    udev.packages = [pkgs.fwupd];
+  };
 }
