6b8827097962f60c78474c9b22668a987a180c38
biking-along <rsw56379@gmail.com>
2025-07-29 16:02:00 -0500

    sdf

--- modules/nixos/gamma/firmware/firmware.nix
+++ modules/nixos/gamma/firmware/firmware.nix
@@ -3,6 +3,10 @@
     enableAllFirmware = true;
     enableRedistributableFirmware = true;
     i2c.enable = true;
+    cpu.x86.msr = {
+      enable = true;
+      settings.allow-writes = "off";
+    };
   };
 
   services = {
