532f11f0f2b60a40f376afe7350118a2f7d7a40e
biking-along <rsw56379@gmail.com>
2025-07-31 08:30:37 -0500

    Configuring pyprland

---
+++ modules/home-manager/gamma/homeFiles.nix
@@ -0,0 +1,37 @@
+{
+  xdg.configFile = {
+    "pyprland.toml" = {
+      enable = true;
+      target = "./hypr/pyprland.toml";
+      text = ''
+        [pyprland]
+        plugins = [
+          "layout_center",
+          "scratchpads",
+          "shift_monitors",
+          "workspaces_follow_focus"
+        ]
+
+        [layout_center]
+        margin = 60
+        offset = [
+          0,
+          30,
+        ]
+        next = "movefocus r"
+        prev = "movefocus l"
+        next2 = "movefocus d"
+        prev2 = "movefocus u"
+
+        [workspaces_follow_focus]
+        max_workspaces = 9
+
+        [scratchpads.term]
+        animation = "fromTop"
+        command = "kitty --class kitty-dropterm"
+        class = "kitty-dropterm"
+        size = "75% 60%"
+      '';
+    };
+  };
+}
