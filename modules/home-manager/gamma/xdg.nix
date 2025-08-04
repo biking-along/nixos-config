32ea287d2426ac02a64b1b1d8695ecfdaf2d0662
biking-along <rsw56379@gmail.com>
2025-08-04 10:41:45 -0500

    sdf

--- modules/home-manager/gamma/xdg.nix
+++ modules/home-manager/gamma/xdg.nix
@@ -1,34 +1,37 @@
-{
+{pkgs, ...}: {
   xdg = {
     enable = true;
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "inode/directory" = [ "yazi.desktop" ];
-        "text/html" = [ "firefox.desktop" ];
-        "text/plain" = [ "nvim.desktop" ];
-        "image/png" = [ "imv.desktop" ];
-        "image/svg" = [ "imv.desktop" ];
-        "image/jpg" = [ "imv.desktop" ];
-        "image/jpeg" = [ "imv.desktop" ];
-        "image/webp" = [ "imv.desktop" ];
-        "image/gif" = [ "imv.desktop" ];
-        "image/bmp" = [ "imv.desktop" ];
-        "video/mp4" = [ "umpv.desktop" ];
-        "video/mkv" = [ "umpv.desktop" ];
-        "video/webm" = [ "umpv.desktop" ];
-        "application/zip" = [ "yazi.desktop" ];
-        "application/pdf" = [ "org.pwmt.zathura.desktop" ];
-        "application/x-extension-htm" = [ "firefox.desktop" ];
-        "application/x-extension-html" = [ "firefox.desktop" ];
-        "application/x-extension-shtml" = [ "firefox.desktop" ];
-        "application/xhtml+xml" = [ "firefox.desktop" ];
-        "application/x-extension-xhtml" = [ "firefox.desktop" ];
-        "application/x-extension-xht" = [ "firefox.desktop" ];
-        "x-scheme-handler/http" = [ "firefox.desktop" ];
-        "x-scheme-handler/https" = [ "firefox.desktop" ];
-        "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+        "inode/directory" = ["yazi.desktop"];
+        "text/html" = ["firefox.desktop"];
+        "text/plain" = ["nvim.desktop"];
+        "image/png" = ["imv.desktop"];
+        "image/svg" = ["imv.desktop"];
+        "image/jpg" = ["imv.desktop"];
+        "image/jpeg" = ["imv.desktop"];
+        "image/webp" = ["imv.desktop"];
+        "image/gif" = ["imv.desktop"];
+        "image/bmp" = ["imv.desktop"];
+        "video/mp4" = ["umpv.desktop"];
+        "video/mkv" = ["umpv.desktop"];
+        "video/webm" = ["umpv.desktop"];
+        "application/zip" = ["yazi.desktop"];
+        "application/pdf" = ["org.pwmt.zathura.desktop"];
+        "application/x-extension-htm" = ["firefox.desktop"];
+        "application/x-extension-html" = ["firefox.desktop"];
+        "application/x-extension-shtml" = ["firefox.desktop"];
+        "application/xhtml+xml" = ["firefox.desktop"];
+        "application/x-extension-xhtml" = ["firefox.desktop"];
+        "application/x-extension-xht" = ["firefox.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/chrome" = ["firefox.desktop"];
       };
     };
+    portal.extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
   };
 }
