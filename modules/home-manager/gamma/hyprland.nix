d7dd3784eacf3f98b87bf144d9cfd76d400fd22f
biking-along <rsw56379@gmail.com>
2025-08-03 21:33:38 -0500

    sdcf

--- modules/home-manager/gamma/hyprland.nix
+++ modules/home-manager/gamma/hyprland.nix
@@ -14,7 +14,7 @@
 
         "uwsm-terminal-service -- kitten desktop-ui run-server"
 
-        "uwsm-app -- kitty --class='kitty-bg' 'cbonsai --life 40 --multiplier 5 --time 0.08 --infinte --live'"
+        "kitty --class='kitty-bg' 'cbonsai --life 40 --multiplier 5 --time 0.08 --infinte --live'"
       ];
       general = {
         gaps_in = 3;
