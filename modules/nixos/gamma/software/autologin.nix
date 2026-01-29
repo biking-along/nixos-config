f771ed3d967199b4775b97a1aec265c56b8b1047
biking-along <rsw56379@gmail.com>
2026-01-29 14:22:39 -0600

    try to fix error

--- modules/nixos/gamma/software/autologin.nix
+++ modules/nixos/gamma/software/autologin.nix
@@ -1,8 +1,6 @@
 {username, ...}: {
   services.getty.autologinUser = "${username}";
   programs.fish.loginShellInit = ''
-    if uwsm check may-start;
-      exec uwsm start default
-    end
+    start-hyprland
   '';
 }
