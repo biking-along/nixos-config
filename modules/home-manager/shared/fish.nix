0331dfa31dff9ef3941461a5d375499113d24945
biking-along <rsw56379@gmail.com>
2026-02-15 22:53:27 -0600

    Add fish alias'

--- modules/home-manager/shared/fish.nix
+++ modules/home-manager/shared/fish.nix
@@ -8,6 +8,8 @@
       cat = "bat";
       man = "batman";
       gg = "lazygit";
+      lambda-dav = "rclone mount --no-check-certificate --vfs-cache-mode writes --dir-cache-time 5s lambda-dav: /home/rw/Network/lambda/";
+      cdn = "cd /home/rw/NixOS/nixos-config/";
     };
   };
 }
