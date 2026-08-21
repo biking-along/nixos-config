d68c97165d489ae3c37055758db69d776a6ba928
bikingalong <bikingalong@pm.me>
2026-08-21 18:16:51 -0500

    Add Nix-search-tv and enable television

--- modules/home-manager/shared/all/nix-search.nix
+++ modules/home-manager/shared/all/nix-search.nix
@@ -1,3 +1,10 @@
 {
-  programs.nix-search-tv.enable = true;
+  programs.nix-search-tv = {
+    enable = true;
+    enableTelevisionIntegration = true;
+  };
+  programs.television = {
+    enable = true;
+    enableFishIntegration = true;
+  };
 }
