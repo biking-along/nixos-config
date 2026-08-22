749954c7d79f5f81dc30715f60597b406b068bd2
bikingalong <94024370+biking-along@users.noreply.github.com>
2026-08-22 00:08:03 -0500

    Update firmware.nix

--- modules/nixos/kappa/firmware/firmware.nix
+++ modules/nixos/kappa/firmware/firmware.nix
@@ -1,4 +1,19 @@
 {
-  hardware.enableAllFirmware = true;
+  hardware = {
+    enableAllFirmware = true;
+    cpu = {
+      intel = {
+        updateMicrocode = true;
+        sgx = {
+          enableDcapCompat = true;
+          provision.enable = true;
+        };
+      };
+      x86.msr = {
+        enable = true;
+        settings.allow-writes = true;
+      };
+    };
+  };
   services.udev.enable = true;
 }
