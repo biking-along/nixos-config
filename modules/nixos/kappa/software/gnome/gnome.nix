3a0ee011bb72e7abd4c783dfbe6351e574e7413b
bikingalong <bikingalong@pm.me>
2026-08-22 19:32:27 -0500

    A

--- modules/nixos/kappa/software/gnome/gnome.nix
+++ modules/nixos/kappa/software/gnome/gnome.nix
@@ -16,7 +16,6 @@
       autoSuspend = true;
     };
   };
-  qt.platformTheme = lib.mkForce "adwaita";
   security.pam.services."rw".enableGnomeKeyring = true;
   environment.shells = with pkgs; [fish];
   services.desktopManager.gnome.enable = true;
