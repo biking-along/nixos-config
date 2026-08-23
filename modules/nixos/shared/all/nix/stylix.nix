7cbd5ecd463cf16a1f23bdd79975e5714e961f1a
biking-along <bikingalong@pm.me>
2026-08-23 00:59:13 -0500

    Add new auth ssh user key

--- modules/nixos/shared/all/nix/stylix.nix
+++ modules/nixos/shared/all/nix/stylix.nix
@@ -14,7 +14,7 @@
     image = ./nix-mocha.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
-    opacity.terminal = 0.8;
+    opacity.terminal = 1.0;
     cursor = {
       package = pkgs.nordzy-cursor-theme;
       name = "Nordzy-catppuccin-mocha-dark";
