694ad3d7fdd0a1a00f4ce75f1d28f07603606c9c
bikingalong <bikingalong@pm.me>
2026-08-30 19:58:20 -0500

    Add gemini api key to age and export to home session variables

--- modules/home-manager/shared/workstation/antigravity-cli.nix
+++ modules/home-manager/shared/workstation/antigravity-cli.nix
@@ -1,4 +1,4 @@
-{
+{config, ...}: {
   age = {
     identityPaths = ["/home/rw/.ssh/id_ed25519"];
     secrets = {
@@ -6,6 +6,9 @@
         file = ../../../../secrets/geminiApiKey.age;
       };
     };
+  };
+  home.sessionVariables = {
+    GEMINI_API_KEY = "(cat ${config.age.secrets.geminiApiKey.path})";
   };
   programs.antigravity-cli = {
     enable = true;
