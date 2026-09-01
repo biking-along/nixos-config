d05dbddf1c9c7281ff61de371fed95a898d20f99
Ryan Williams <bikingalong@pm.me>
2026-09-01 15:39:13 -0500

    More traefik debugging

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,10 +5,10 @@ let
   kappa = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDoN0lsT2cyxUgcx/mKe/WZM9BGTxd50azlFLsB08Iw1";
   fleet = [lambda gamma kappa omicron];
 in {
-  "traefik.env.age".publicKeys = [fleet];
-  "authentik.env.age".publicKeys = [fleet];
-  "grafanaSecret.age".publicKeys = [fleet];
-  "paperlessPass.age".publicKeys = [fleet];
-  "ryan01EmailPass.age".publicKeys = [fleet];
-  "geminiApiKey.age".publicKeys = [fleet];
+  "traefik.env.age".publicKeys = fleet;
+  "authentik.env.age".publicKeys = fleet;
+  "grafanaSecret.age".publicKeys = fleet;
+  "paperlessPass.age".publicKeys = fleet;
+  "ryan01EmailPass.age".publicKeys = fleet;
+  "geminiApiKey.age".publicKeys = fleet;
 }
