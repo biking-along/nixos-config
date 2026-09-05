8fdc75b0a8fb34fc240484898cef53229e0183a8
biking-along <bikingalong@pm.me>
2026-09-05 15:02:11 -0500

    Working on epsilon config

--- flake.nix
+++ flake.nix
@@ -139,17 +139,16 @@
             stylix.nixosModules.stylix
             nvf.nixosModules.default
             agenix.nixosModules.default
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.extraSpecialArgs = {inherit specialArgs;};
-              home-manager.users.${username} = {
-                imports = [
-                  ./modules/home-manager/${host}
-                  ./modules/home-manager/shared
-                  agenix.homeManagerModules.default
-                ];
-              };
-            }
+            # home-manager.nixosModules.home-manager
+            # {
+            #   home-manager.users.${username} = {
+            #     imports = [
+            #       ./modules/home-manager/${host}
+            #       # ./modules/home-manager/shared
+            #       agenix.homeManagerModules.default
+            #     ];
+            #   };
+            # }
           ];
         };
 
