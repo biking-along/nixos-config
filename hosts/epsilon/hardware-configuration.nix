9cf7727506c83e152c91dca2fdd4de120d3f7e74
rw <bikingalong@pm.me>
2026-09-05 10:37:31 -0500

    Add default and hardware config for epsilon

--- hosts/epsilon/hardware-configuration.nix
+++ hosts/epsilon/hardware-configuration.nix
@@ -1,33 +1,36 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
 {
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  ...
-}: {
-  imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
-  ];
-  boot = {
-    initrd = {
-      availableKernelModules = ["ahci" "virtio_pci" "virtio_scsi" "xhci_pci" "sr_mod" "virtio_blk"];
-      kernelModules = [];
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "ahci" "virtio_pci" "virtio_scsi" "xhci_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/3a2be263-8394-400c-b84c-91b4c407cc5c";
+      fsType = "ext4";
     };
-    kernelModules = [];
-    extraModulePackages = [];
-  };
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/3a2be263-8394-400c-b84c-91b4c407cc5c";
-    fsType = "ext4";
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by/uuid/F595-37E8";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/f3894b08-7901-4223-9ba8-4f6c0bdb997b";}
-  ];
-  networking.useDHCP = lib.mkdefault true;
-  nixpkgs.hostPlatform = lib.mkdefault "x86_64-linux";
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/F595-37E8";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/f3894b08-7901-4223-9ba8-4f6c0bdb997b"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
