52019d92f948f4f4b376d3bc32154ba881278cae
rw <bikingalong@pm.me>
2026-09-16 10:20:49 -0500

    Change file permissions

--- modules/nixos/epsilon/software/headplane.nix
+++ modules/nixos/epsilon/software/headplane.nix
@@ -1,4 +1,8 @@
-{
+{config, ...}: {
+  age.secrets."headplaneCookieSecret" = {
+    file = ../../../../secrets/headplaneCookieSecret.age;
+    mode = "644";
+  };
   services.headplane = {
     enable = true;
     settings = {
@@ -6,7 +10,8 @@
       server = {
         base_url = "https://headplane.bikingalong.com";
         host = "0.0.0.0";
-        port = "8082";
+        port = 8082;
+        cookie_secret_path = config.age.secrets."headplaneCookieSecret".path;
       };
     };
   };
