0b7deceeef58208a03d6973d5205f2551e899b95
rw <bikingalong@pm.me>
2026-09-18 15:06:13 -0500

    Attempt to fix invalid oidc uri for headplane

--- modules/nixos/epsilon/software/headplane.nix
+++ modules/nixos/epsilon/software/headplane.nix
@@ -22,7 +22,7 @@
         api_key_path = config.age.secrets."hsApiKey".path;
       };
       server = {
-        base_url = "https://headplane.bikingalong.com";
+        base_url = "https://headscale.bikingalong.com/admin";
         host = "0.0.0.0";
         port = 8082;
         cookie_secret_path = config.age.secrets."headplaneCookieSecret".path;
