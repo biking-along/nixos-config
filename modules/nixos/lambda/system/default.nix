d4c2267537ffb2818ecfc4c0c437aa74b3397360
biking-along <bikingalong@pm.me>
2026-09-20 00:02:16 -0500

    Add lambda settings for 1060 install

--- modules/nixos/lambda/system/default.nix
+++ modules/nixos/lambda/system/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./bootloader.nix
+    ./nvidia.nix
   ];
 }
---
+++ modules/nixos/lambda/system/nvidia.nix
@@ -0,0 +1,14 @@
+{
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  };
+}
