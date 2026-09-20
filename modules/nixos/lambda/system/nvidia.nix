d41136a9f638c810f16ae3bc5481a5f981742829
Ryan Williams <bikingalong@pm.me>
2026-09-20 00:27:08 -0500

    Nvidia

--- modules/nixos/lambda/system/nvidia.nix
+++ modules/nixos/lambda/system/nvidia.nix
@@ -1,15 +1,16 @@
 {
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
   };
 
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
+    open = false;
+    nvidiaPersistenced = true;
     modesetting.enable = true;
     nvidiaSettings = true;
+    powerManagement.enable = true;
   };
   boot.blacklistedKernelModules = ["nouveau"];
 }
